5639d7f75d5db9f670e96956a51252b784a9bdc9
Dannyung50 <olaleyedaniel16@gmail.com>
2023-10-06 01:04:01 +0100

    Initial Commit

---
+++ UnitCoverter/UnitCoverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitCoverterApp.swift
+//  UnitCoverter
+//
+//  Created by Olaleye Daniel Ifeoluwa on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitCoverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
